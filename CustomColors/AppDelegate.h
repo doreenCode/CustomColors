87f964f6e8fc31f533115b57250c89546ad12673
DCT <doreen@steerlist.com>
2015-09-14 10:47:50 -0700

    Initial Commit

---
+++ CustomColors/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomColors
+//
+//  Created by Doreen Theverapperuma on 9/14/15.
+//  Copyright © 2015 DCT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
